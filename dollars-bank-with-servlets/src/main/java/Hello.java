bd781653337abe5322deae79b94f686ccc1010a3
Gabriel Ortiz <igabriel661@hotmail.com>
2022-10-18 10:58:59 -0700

    Tomcat server working and Hello class is just a test (can delete).

---
+++ dollars-bank-with-servlets/src/main/java/Hello.java
@@ -0,0 +1,36 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	public Hello() {}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		pw.print("<html><body>");
+		pw.print("<h3>Hello Servlet</h3>");
+		pw.print("<body><html>");
+		
+	}
+
+}
